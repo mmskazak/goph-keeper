1c0ee1ee42e6665f75ae5a36206cd3baea1fd306
mmskazak <mmskazak@gmail.com>
2024-10-08 16:32:47 +0300

    upd

--- cmd/goph-keeper/main.go
+++ cmd/goph-keeper/main.go
@@ -2,16 +2,10 @@ package goph_keeper
 
 import (
 	"context"
-	"fmt"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"gophKeeper/internal/app"
 	"gophKeeper/internal/config"
 	"gophKeeper/internal/logger"
 	"gophKeeper/internal/postgres"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
@@ -49,41 +43,4 @@ func main() {
 	if err != nil {
 		logger.Log.Errorf("Ошибка запусака приложения: %v", err)
 	}
-}
-
-func runApp(
-	ctx context.Context,
-	cfg *config.Config,
-	pool *pgxpool.Pool,
-	shutdownDuration time.Duration,
-) error {
-	defer func() {
-		pool.Close()
-	}()
-
-	newApp := app.NewApp(ctx, cfg)
-	err := newApp.Start()
-	if err != nil {
-		return fmt.Errorf("error start app: %w", err)
-	}
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	select {
-	case <-quit: // Ожидание сигнала завершения
-		logger.Log.Infoln("Получен сигнал завершения, остановка сервера...")
-	case <-ctx.Done(): // Завершение по контексту
-		logger.Log.Infoln("Контекст завершён, остановка сервера...")
-	}
-
-	ctxShutdown, cancel := context.WithTimeout(context.Background(), shutdownDuration)
-	defer cancel()
-
-	if err := newApp.Stop(ctxShutdown); err != nil {
-		return fmt.Errorf("error stop app: %w", err)
-	}
-	logger.Log.Infoln("Приложение завершило работу.")
-
-	return nil
 }
