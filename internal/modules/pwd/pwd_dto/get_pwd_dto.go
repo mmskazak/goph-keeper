8338eeee3c84639ccc67f50d5d07fe3ec70860b3
mmskazak <mmskazak@gmail.com>
2024-10-17 13:06:57 +0300

    upd

--- internal/modules/pwd/pwd_dto/get_pwd_dto.go
+++ internal/modules/pwd/pwd_dto/get_pwd_dto.go
@@ -10,27 +10,39 @@ import (
 )
 
 type GetPwdDTO struct {
-	ID     string `json:"id"`      //ID пароля в базе данных
-	UserID string `json:"user_id"` //Идентификатор пользователя
+	PwdID  string `json:"pwd_id"`  // ID пароля в базе данных
+	UserID int    `json:"user_id"` // Идентификатор пользователя
 }
 
 func GetPwdDTOFromHTTP(r *http.Request) (GetPwdDTO, error) {
+	// Читаем тело запроса
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		return GetPwdDTO{}, fmt.Errorf("reading body registration: %w", err)
 	}
+	defer r.Body.Close() // Закрываем тело запроса после чтения
+
 	var getPwdDTO GetPwdDTO
+	// Декодируем JSON в структуру
 	err = json.Unmarshal(data, &getPwdDTO)
 	if err != nil {
 		return GetPwdDTO{}, fmt.Errorf("unmarshalling body registration: %w", err)
 	}
-	return getPwdDTO, nil
+
+	// Извлекаем userID из контекста
+	userID, err := getUserIDFromContext(r.Context())
+	if err != nil {
+		return GetPwdDTO{}, err // Возвращаем ошибку, если userID не найден
+	}
+
+	getPwdDTO.UserID = userID // Устанавливаем userID в структуру
+	return getPwdDTO, nil     // Возвращаем структуру и nil (без ошибки)
 }
 
 // GetUserIDFromContext Функция для извлечения userID из контекста
-func GetUserIDFromContext(ctx context.Context) (int, error) {
+func getUserIDFromContext(ctx context.Context) (int, error) {
 	if userID, ok := ctx.Value(auth_middleware.UserIDKey).(int); ok {
-		return userID, nil
+		return userID, nil // Возвращаем userID
 	}
-	return 0, fmt.Errorf("userID not found in context")
+	return 0, fmt.Errorf("userID not found in context") // Ошибка, если не найден
 }
