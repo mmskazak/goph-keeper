d53cf6288d841dbf09871652ed07497d45eadf7e
mmskazak <mmskazak@gmail.com>
2024-11-02 16:51:35 +0300

    upd

--- internal/modules/file/filegrpc/file_service.go
+++ internal/modules/file/filegrpc/file_service.go
@@ -28,14 +28,12 @@ func NewFileGRPCServer(fileService fileservices.IFileService, secretKey string) 
 }
 
 // SaveFile сохраняет файл на сервере
-func (s *FileGRPCServer) SaveFile(ctx context.Context, req *proto.SaveFileRequest) (*proto.SaveFileResponse, error) {
-	// Проверка и извлечение userID из JWT
+func (s *FileGRPCServer) SaveFile(ctx context.Context, req *proto.SaveFileRequest) (*proto.BasicResponse, error) {
 	userID, err := authjwtservice.ParseAndValidateToken(req.Jwt, s.secretKey)
 	if err != nil {
 		return nil, fmt.Errorf("unauthorized: %w", err)
 	}
 
-	// Создание DTO для сохранения файла
 	saveFileDTO := filedto.SaveFileDTO{
 		UserID:      userID,
 		Title:       req.Title,
@@ -43,91 +41,81 @@ func (s *FileGRPCServer) SaveFile(ctx context.Context, req *proto.SaveFileReques
 		FileData:    req.FileData,
 	}
 
-	// Сохранение файла
 	if err := s.fileService.SaveFile(ctx, saveFileDTO); err != nil {
 		return nil, fmt.Errorf("failed to save file: %w", err)
 	}
 
-	return &proto.SaveFileResponse{Message: "File saved successfully"}, nil
+	return &proto.BasicResponse{Status: "success", Message: "File saved successfully"}, nil
 }
 
 // GetFile возвращает файл пользователю
 func (s *FileGRPCServer) GetFile(ctx context.Context, req *proto.GetFileRequest) (*proto.GetFileResponse, error) {
-	// Проверка и извлечение userID из JWT
 	userID, err := authjwtservice.ParseAndValidateToken(req.Jwt, s.secretKey)
 	if err != nil {
 		return nil, fmt.Errorf("unauthorized: %w", err)
 	}
 
-	// Создание DTO для получения файла
 	getFileDTO := filedto.GetFileDTO{
 		UserID: userID,
 		FileID: req.FileId,
 	}
 
-	// Получение файла
-	tempFilePath, err := s.fileService.GetFile(ctx, getFileDTO)
+	tempFilePath, fileInfo, err := s.fileService.GetFile(ctx, getFileDTO)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get file: %w", err)
 	}
 
-	// Чтение файла и возврат данных в ответе
 	fileData, err := os.ReadFile(tempFilePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read file data: %w", err)
 	}
 
 	return &proto.GetFileResponse{
-		FileData: fileData,
-		Title:    getFileDTO.Title, // Предполагается, что заголовок был установлен
+		FileId:      fileInfo.FileID,
+		Title:       fileInfo.Title,
+		Description: fileInfo.Description,
+		FileData:    fileData,
 	}, nil
 }
 
 // DeleteFile удаляет файл с сервера
-func (s *FileGRPCServer) DeleteFile(ctx context.Context, req *proto.DeleteFileRequest) (*proto.DeleteFileResponse, error) {
-	// Проверка и извлечение userID из JWT
+func (s *FileGRPCServer) DeleteFile(ctx context.Context, req *proto.DeleteFileRequest) (*proto.BasicResponse, error) {
 	userID, err := authjwtservice.ParseAndValidateToken(req.Jwt, s.secretKey)
 	if err != nil {
 		return nil, fmt.Errorf("unauthorized: %w", err)
 	}
 
-	// Создание DTO для удаления файла
 	deleteFileDTO := filedto.DeleteFileDTO{
 		UserID: userID,
 		FileID: req.FileId,
 	}
 
-	// Удаление файла
 	if err := s.fileService.DeleteFile(ctx, deleteFileDTO); err != nil {
 		return nil, fmt.Errorf("failed to delete file: %w", err)
 	}
 
-	return &proto.DeleteFileResponse{Message: "File deleted successfully"}, nil
+	return &proto.BasicResponse{Status: "success", Message: "File deleted successfully"}, nil
 }
 
 // GetAllFiles возвращает список всех файлов пользователя
 func (s *FileGRPCServer) GetAllFiles(ctx context.Context, req *proto.GetAllFilesRequest) (*proto.GetAllFilesResponse, error) {
-	// Проверка и извлечение userID из JWT
 	userID, err := authjwtservice.ParseAndValidateToken(req.Jwt, s.secretKey)
 	if err != nil {
 		return nil, fmt.Errorf("unauthorized: %w", err)
 	}
 
-	// Создание DTO для запроса всех файлов
 	getAllFilesDTO := filedto.AllFilesDTO{
 		UserID: userID,
 	}
 
-	// Получение списка всех файлов
 	files, err := s.fileService.GetAllFiles(ctx, getAllFilesDTO)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get all files: %w", err)
 	}
 
-	// Формирование ответа
-	var fileItems []*proto.FileItem
+	var fileItems []*proto.FileInfo
 	for _, file := range files {
-		fileItems = append(fileItems, &proto.FileItem{
+		fileItems = append(fileItems, &proto.FileInfo{
 			FileId:      file.FileID,
 			Title:       file.Title,
 			Description: file.Description,
