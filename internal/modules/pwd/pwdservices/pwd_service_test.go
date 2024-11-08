29801a4f6802cd2e50b78bb40e8395570a4630d8
mmskazak <mmskazak@gmail.com>
2024-11-08 11:39:11 +0300

    add test

--- internal/modules/pwd/pwdservices/pwd_service_test.go
+++ internal/modules/pwd/pwdservices/pwd_service_test.go
@@ -187,3 +187,49 @@ func TestPwdService_GetAllPasswords(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "mypassword123", listPasswords[0].Credentials.Password)
 }
+
+func TestPwdService_GetAllPasswordsErrUnmarshallingCredentials(t *testing.T) {
+	ctx := context.Background()
+	mockPool := new(mocks.MockDatabase)
+	mockRows := new(mocks.MockRows)
+	// Строка длиной 32 символа
+	strKey := "MySecretEncryptionKey1234567890a"
+	// Преобразуем строку в массив байтов
+	var key [32]byte
+	copy(key[:], strKey)
+	s := NewPwdService(mockPool, key, zap.NewNop().Sugar())
+	dto := pwddto.AllPwdDTO{
+		UserID: 1,
+	}
+	mockPool.On("Query", ctx,
+		mock.Anything,
+		mock.Anything,
+	).Return(mockRows, nil)
+
+	mockRows.On("Next").Return(true).Once()  // Первый вызов вернет true
+	mockRows.On("Next").Return(false).Once() // Второй вызов вернет false
+	mockRows.On("Err").Return(nil).Once()
+	mockRows.On("Scan",
+		mock.AnythingOfType("*string"),
+		mock.AnythingOfType("*string"),
+		mock.AnythingOfType("*[]uint8"),
+	).
+		Run(func(args mock.Arguments) {
+			id, ok := args.Get(0).(*string)
+			if ok {
+				*id = "1"
+			}
+			title, ok := args.Get(1).(*string)
+			if ok {
+				*title = "http://google.com"
+			}
+			credential, ok := args.Get(2).(*[]byte)
+			if ok {
+				*credential = []byte("login\": \"myemail@example.com\", \"password\": \"17c5d99202bd0e1141d3dcee:2ac2a44261925c17213821b8c4")
+			}
+		}).Return(nil)
+
+	listPasswords, err := s.GetAllPasswords(ctx, &dto)
+	assert.EqualError(t, err, "error unmarshalling credentials: invalid character 'l' looking for beginning of value")
+	assert.Equal(t, []pwddto.ResponsePwdDTO{}, listPasswords)
+}
